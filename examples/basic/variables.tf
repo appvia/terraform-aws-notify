f8b07408d170f9891fb8ec2e70ed5bcfffa869e3
Rohith Jayawardene <gambol99@gmail.com>
2026-06-25 18:39:25 +0100

    fix(examples): resolved the unused variable in the examples

--- examples/basic/variables.tf
+++
@@ -1,6 +0,0 @@
-
-variable "slack_webhook_url" {
-  description = "The Slack webhook URL to send notifications to"
-  type        = string
-  default     = "https://hooks.slack.com/services/XXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
-}
